3bf2f36689abbb82453d8bc8512099de5fa3cc12
BadrKilou <Bobbyevin07@gmail.com>
2020-05-22 17:31:42 +0200

    Updates

---
+++ precache-manifest.527d513631e5e8616be246d8eadf7345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e502abdc9bca62fa882deac70108791c",
+    "url": "/Monsters-Project/index.html"
+  },
+  {
+    "revision": "98ca780b8561a4ee90dd",
+    "url": "/Monsters-Project/static/css/main.cb0763b7.chunk.css"
+  },
+  {
+    "revision": "01695adb10bdd1a499dc",
+    "url": "/Monsters-Project/static/js/2.49ac2006.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Project/static/js/2.49ac2006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ca780b8561a4ee90dd",
+    "url": "/Monsters-Project/static/js/main.d7da5817.chunk.js"
+  },
+  {
+    "revision": "d1e39a31f3b2cb347f23",
+    "url": "/Monsters-Project/static/js/runtime-main.5c60a4c7.js"
+  }
+]);
